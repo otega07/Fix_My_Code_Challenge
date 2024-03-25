22da03e169667450fefed63917cc312aafe4ee13
otega07 <okiriotega@gmail.com>
2024-03-25 01:32:32 +0100

    fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -8,13 +8,13 @@ import sys
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
-    
+
     - For multiples of three print "Fizz" instead of the number and for
       multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
-        return
+        raise ValueError("The input number must be a positive integer.")
 
     tmp_result = []
     for i in range(1, n + 1):
@@ -30,11 +30,19 @@ def fizzbuzz(n):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
+    if len(sys.argv) != 2:
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    try:
+        number = int(sys.argv[1])
+    except ValueError:
+        print("Error: The provided number is not a valid integer.")
+        sys.exit(1)
+
+    try:
+        fizzbuzz(number)
+    except ValueError as e:
+        print("Error:", str(e))
+        sys.exit(1)
